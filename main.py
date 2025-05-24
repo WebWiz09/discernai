6891105a1d3e4987bc87024f22b34ec1636ab4ce
Henry Nnamene <113104787+WebWiz09@users.noreply.github.com>
2025-05-25 00:32:34 +0100

    Update main.py
    used an env for API key

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
 from groq import Groq
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/fact-check": {"origins": ["https://discernai.vercel.app", "https://discernai.vercel.app/main.html"]}})
 
 client = Groq(
-    api_key="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",
+    api_key=os.getenv("GROQ_API_KEY")
 )
-
 @app.route('/fact-check', methods=['POST'])
 def fact_check():
     data = request.json
